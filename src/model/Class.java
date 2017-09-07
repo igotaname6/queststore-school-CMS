242729ceeb7400a04eb5db6b2711fab91f3893d7
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-07 17:01:45 +0200

    Add instance attributes to constructor

--- src/model/Class.java
+++ src/model/Class.java
@@ -3,8 +3,12 @@ package model;
 public class Class {
 
     private String name;
-    
+    private ArrayList<Codecooler> members;
+    private ArrayList<Mentor> mentor;
+
     public Class(String name) {
         this.name = name;
+        this.members = new ArrayList<>();
+        this.mentors = new ArrayList<>();
     }
 }
