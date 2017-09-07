1d2d33dd7077fe3ee4c954c7347f00330df37a11
Fesh <maciej.ukowski@gmail.com>
2017-09-07 14:31:58 +0200

    Add getters and setters to quest class

--- src/model/Quest.java
+++ src/model/Quest.java
@@ -2,9 +2,9 @@ package model;
 
 public class Quest {
 
-    String name;
-    String description;
-    Integer coinReward;
+    private String name;
+    private String description;
+    private Integer coinReward;
 
     public Quest() {
 
@@ -19,4 +19,28 @@ public class Quest {
         this.description = description;
         this.coinReward = coinReward;
     }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCoinReward() {
+        return this.coinReward;
+    }
+
+    public void setCoinReward(Integer amount) {
+        this.coinReward = amount;
+    } 
 }
