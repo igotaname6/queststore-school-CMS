4385c6bd064b49efff80e98573d5f62eebccb001
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-07 15:48:17 +0200

    Add Mentor class constructor

---
+++ src/model/Mentor.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Mentor extends AbstractUser {
+
+    public Mentor(String name, String surname, String email, String password) {
+        super(name, surname, email, password);
+    }
+}
