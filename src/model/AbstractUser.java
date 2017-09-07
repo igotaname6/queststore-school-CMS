7e1ec4bb38757c1e9dd1b0c1fa0183990656ab12
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-07 14:33:11 +0200

    Add AbstractUser constructor

--- src/model/AbstractUser.java
+++ src/model/AbstractUser.java
@@ -6,5 +6,10 @@ public abstract class AbstractUser {
     private String email;
     private String password;
 
-
+    public AbstractUser(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
 }
