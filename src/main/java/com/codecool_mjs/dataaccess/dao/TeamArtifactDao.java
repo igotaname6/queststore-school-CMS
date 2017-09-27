3c42630639e338ffeb73aed680021d92e5c95ed1
Fesh <maciej.ukowski@gmail.com>
2017-09-27 17:00:47 +0200

    Add createObject and getQuary methods

--- src/main/java/com/codecool_mjs/dataaccess/dao/TeamArtifactDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/TeamArtifactDao.java
@@ -1,4 +1,25 @@
 package com.codecool_mjs.dataaccess.dao;
 
-public class TeamArtifactDao {
+import com.codecool_mjs.model.Artifact;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TeamArtifactDao extends Dao<Artifact> {
+    private static String QUARY = "SELECT * FROM artifacts WHERE type = 'team'";
+
+    Artifact createObject(ResultSet results) throws SQLException {
+
+        String name = results.getString("name");
+        String description = results.getString("description");
+        Integer cost = results.getInt("cost");
+
+        Artifact artifact = new Artifact(name, description, cost);
+
+        return artifact;
+    }
+
+    String getQuery() {
+        return QUARY;
+    }
 }
