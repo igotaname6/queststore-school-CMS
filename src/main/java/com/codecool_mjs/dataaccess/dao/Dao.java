f92d1e19e68977802a48a0b92b135ea2750eaa30
Fesh <maciej.ukowski@gmail.com>
2017-09-27 16:01:12 +0200

    Add getAll method

--- src/main/java/com/codecool_mjs/dataaccess/dao/Dao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/Dao.java
@@ -1,18 +1,41 @@
 package com.codecool_mjs.dataaccess.dao;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 abstract public class Dao<T> implements DaoInterface<T> {
 
     abstract T createObject(ResultSet results) throws SQLException;
-    abstract List<T> prepareList(ResultSet results) throws SQLException;
+//    abstract List<T> prepareList(ResultSet results) throws SQLException;
     abstract String getQuery();
 
     @Override
     public List<T> getAll() {
-        return null;
+
+        ArrayList<T> resultsList = null;
+
+        try {
+            Connection conn = ConnectionProvider.getConnection();
+            Statement stnt = conn.createStatement();
+            ResultSet results = stnt.executeQuery(getQuery());
+
+            resultsList = new ArrayList<>();
+
+            while (results.next()) {
+
+                T object = createObject(results);
+                resultsList.add(object);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultsList;
     }
 
     @Override
