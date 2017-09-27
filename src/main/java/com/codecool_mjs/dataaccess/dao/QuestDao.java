f61ae27c81412ade945e9eb4ae62894d13bf165c
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-27 16:26:13 +0200

    Add createObject method

--- src/main/java/com/codecool_mjs/dataaccess/dao/QuestDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/QuestDao.java
@@ -1,11 +1,27 @@
 package com.codecool_mjs.dataaccess.dao;
 
+
 import com.codecool_mjs.model.Quest;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class QuestDao extends Dao<Quest>{
 
     private static String QUERY = "SELECT * FROM quests WHERE type = \"single\";";
 
+    @Override
+    Quest createObject(ResultSet results) throws SQLException {
+
+        String name = results.getString("name");
+        String description = results.getString("description");
+        String reward = results.getString("reward");
+
+        return Quest(name, description, reward);
+    }
+
+
 }
+
+
 
 
