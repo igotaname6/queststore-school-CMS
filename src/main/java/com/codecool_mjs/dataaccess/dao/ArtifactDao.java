105da56804faa5dde238fa62e15bb96888655320
Fesh <maciej.ukowski@gmail.com>
2017-09-27 16:52:14 +0200

    Change in createObject method

--- src/main/java/com/codecool_mjs/dataaccess/dao/ArtifactDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/ArtifactDao.java
@@ -7,17 +7,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ArtifactDao extends Dao<Artifact> {
-    private static String QUARY = "SELECT * FROM artifacts";
+    private static String QUARY = "SELECT * FROM artifacts WHERE type = 'single'";
 
     Artifact createObject(ResultSet results) throws SQLException {
 
-        Integer id = results.getInt("id");
         String name = results.getString("name");
         String description = results.getString("description");
         Integer cost = results.getInt("cost");
-        String type = results.getString("type");
 
-        Artifact artifact = new Artifact(id, name, description, cost, type);
+        Artifact artifact = new Artifact(name, description, cost);
 
         return artifact;
     }
