06435466bfdb24eced933fd1441af8ab59018ba9
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-27 15:50:19 +0200

    Fill createObject method

--- src/main/java/com/codecool_mjs/dataaccess/dao/CodecoolerDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/CodecoolerDao.java
@@ -1,16 +1,29 @@
 package com.codecool_mjs.dataaccess.dao;
 
+import com.codecool_mjs.model.Codecooler;
 import com.codecool_mjs.model.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CodecoolerDao extends Dao<User> {
+
         private static String QUERY = "SELECT * FROM users WHERE profession = \"codecooler\";";
 
         @Override
         User createObject(ResultSet results) throws SQLException {
-            return null;
+
+            User codecooler = null;
+
+            Integer id = results.getInt("id");
+            String name = results.getString("name");
+            String surename = results.getString("surename");
+            String email = results.getString("email");
+            String password = results.getString("password");
+
+            codecooler = new Codecooler(id, name, surename, email, password);
+
+            return codecooler;
         }
 
         @Override
