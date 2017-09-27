6bfbef8c27a7127b51a844ef950098844f55dd84
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-09-27 14:30:31 +0200

    create MentorDao class

--- src/main/java/com/codecool_mjs/dataaccess/dao/Dao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/Dao.java
@@ -9,4 +9,34 @@ abstract public class Dao<T> implements DaoInterface<T> {
     abstract T createObject(ResultSet results) throws SQLException;
     abstract List<T> prepareList(ResultSet results) throws SQLException;
     abstract String getQuery();
+
+    @Override
+    public List<T> getAll() {
+        return null;
+    }
+
+    @Override
+    public <U> List<T> getBy() {
+        return null;
+    }
+
+    @Override
+    public T getById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean update(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean insert(T t) {
+        return false;
+    }
 }
