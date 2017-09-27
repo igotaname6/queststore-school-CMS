11fb2225d00f0782d99a677f8636c515d0e84f2e
Fesh <maciej.ukowski@gmail.com>
2017-09-27 16:04:30 +0200

    Add methods testing statements

--- src/main/java/com/codecool_mjs/App.java
+++ src/main/java/com/codecool_mjs/App.java
@@ -1,5 +1,12 @@
 package com.codecool_mjs;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+import com.codecool_mjs.dataaccess.dao.Dao;
+import com.codecool_mjs.dataaccess.dao.MentorDao;
+import com.codecool_mjs.model.User;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        List<User> lista;
         System.out.println( "Hello World!" );
+        ConnectionProvider.getConnection();
+
+        Dao dao = new MentorDao();
+        lista = dao.getAll();System.out.println(lista);
     }
 }
