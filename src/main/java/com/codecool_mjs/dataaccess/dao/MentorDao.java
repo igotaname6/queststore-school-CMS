e8228ac7dee94fce945e5fc4acb7a7f0285b4b5b
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-09-27 18:03:42 +0200

    add constructor to  MentorDao

--- src/main/java/com/codecool_mjs/dataaccess/dao/MentorDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/MentorDao.java
@@ -3,12 +3,17 @@ package com.codecool_mjs.dataaccess.dao;
 import com.codecool_mjs.model.Mentor;
 import com.codecool_mjs.model.User;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
 public class MentorDao extends Dao<User> {
     private static String QUERY = "SELECT * FROM users WHERE profession = 'mentor'";
+
+    public MentorDao(Connection connection) {
+        super(connection);
+    }
 
     @Override
     User createObject(ResultSet results) throws SQLException {
@@ -28,5 +33,10 @@ public class MentorDao extends Dao<User> {
     @Override
     String getQuery() {
         return QUERY;
+    }
+
+    @Override
+    String getIdQuery() {
+        return null;
     }
 }
