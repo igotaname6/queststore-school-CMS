9699f4cdb031e7c5d61fb53ad2c4bec283d38f5a
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-27 13:48:52 +0200

    Create ConnectionProvider constructor

--- src/main/java/com/codecool_mjs/dataaccess/ConnectionProvider.java
+++ src/main/java/com/codecool_mjs/dataaccess/ConnectionProvider.java
@@ -1,4 +1,21 @@
 package com.codecool_mjs.dataaccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionProvider {
+
+    private static Connection connection = createConnection();
+    public static final String URL = "jdbc:sqlite:data.db";
+    public static final String DRIVER_CLASS = "org.sqlite.JDBC";
+
+    private ConnectionProvider() {
+
+        try {
+            Class.forName(DRIVER_CLASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
