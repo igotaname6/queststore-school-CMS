4eefebc6c14abf22919094e973404128820ad754
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-27 15:33:48 +0200

    Add class architecture and QUERY field

--- src/main/java/com/codecool_mjs/dataaccess/dao/CodecoolerDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/CodecoolerDao.java
@@ -1,4 +1,22 @@
 package com.codecool_mjs.dataaccess.dao;
 
-public class CodecoolerDao {
-}
+import com.codecool_mjs.model.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CodecoolerDao extends Dao<User> {
+        private static String QUERY = "SELECT * FROM users WHERE profession = \"codecooler\";";
+
+        @Override
+        User createObject(ResultSet results) throws SQLException {
+            return null;
+        }
+
+        @Override
+        String getQuery() {
+            return QUERY;
+        }
+    }
+
+
