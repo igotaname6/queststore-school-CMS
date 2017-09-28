dadfc4bfeb971455f074b7b0d51559eb8970bf2f
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-28 13:45:51 +0200

    Add imports

--- src/main/java/com/codecool_mjs/controller/MentorController.java
+++ src/main/java/com/codecool_mjs/controller/MentorController.java
@@ -1,10 +1,14 @@
 package com.codecool_mjs.controller;
 
+import com.codecool_mjs.dataaccess.dao.Dao;
+import com.codecool_mjs.dataaccess.dao.MentorDao;
 import com.codecool_mjs.model.Mentor;
+import com.codecool_mjs.model.User;
 
 public class MentorController {
 
-    public Mentor showMentorsAction() {
+    private Dao<User> dao = new MentorDao(new Dao.getConnection());
 
+    public Mentor showMentorsAction() {
     }
 }
