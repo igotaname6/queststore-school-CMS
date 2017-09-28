4315ae335e447525c35044bb419ed33ec1dc12d7
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-28 16:53:19 +0200

    Create constructor

--- src/main/java/com/codecool_mjs/controller/MentorMenuController.java
+++ src/main/java/com/codecool_mjs/controller/MentorMenuController.java
@@ -1,4 +1,16 @@
 package com.codecool_mjs.controller;
 
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import java.sql.SQLException;
+
 public class MentorMenuController {
+
+    private CodecoolerController codecoolerController;
+    private boolean isRunning;
+
+    public MentorMenuController() throws SQLException{
+        this.codecoolerController = new CodecoolerController();
+        this.isRunning = true;
+    }
 }
