66ef068bfa6853662ac031942f189c4fe8d6c5e8
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-28 14:07:31 +0200

    Add printAllMentors method

--- src/main/java/com/codecool_mjs/view/MentorView.java
+++ src/main/java/com/codecool_mjs/view/MentorView.java
@@ -1,4 +1,17 @@
 package com.codecool_mjs.view;
 
+import com.codecool_mjs.model.User;
+
+import java.util.List;
+
 public class MentorView {
+
+    public static void printAllMentors(List<User> mentors) {
+
+        for (User mentor : mentors) {
+            String data = String.format("%d. %s %s || %s", mentor.getId(), mentor.getFullName(), mentor.getEmail());
+            System.out.println(data);
+        }
+    }
 }
+
