aef1f382b26f5ddcdda18d89e57e1881c7c3f4b0
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-28 14:08:25 +0200

    Delete useless import

--- src/main/java/com/codecool_mjs/controller/MentorController.java
+++ src/main/java/com/codecool_mjs/controller/MentorController.java
@@ -2,7 +2,6 @@ package com.codecool_mjs.controller;
 
 import com.codecool_mjs.dataaccess.dao.Dao;
 import com.codecool_mjs.dataaccess.dao.MentorDao;
-import com.codecool_mjs.model.Mentor;
 import com.codecool_mjs.model.User;
 import com.codecool_mjs.view.MentorView;
 
