eab379cc96735f84e86dc416bdccde91ce6552d0
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-28 16:46:36 +0200

    Add printAllCodecoolers method

--- src/main/java/com/codecool_mjs/view/CodecoolerView.java
+++ src/main/java/com/codecool_mjs/view/CodecoolerView.java
@@ -1,4 +1,16 @@
 package com.codecool_mjs.view;
 
+import com.codecool_mjs.model.User;
+
+import java.util.List;
+
 public class CodecoolerView {
+
+    public static void printAllCodecoolers(List<User> codecoolers) {
+
+        for (User codecooler : codecoolers) {
+            String data = String.format("%d. %s || %s", codecooler.getId(), codecooler.getFullName(), codecooler.getEmail());
+            System.out.println(data);
+        }
+    }
 }
