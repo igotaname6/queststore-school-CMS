bc065c906ede8f6210fe24fcbaa59b770362f7b1
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-28 14:18:17 +0200

    Add printAdminMenu method

--- src/main/java/com/codecool_mjs/view/AdminMenuView.java
+++ src/main/java/com/codecool_mjs/view/AdminMenuView.java
@@ -1,4 +1,24 @@
 package com.codecool_mjs.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
+import static java.lang.System.out;
+
 public class AdminMenuView {
+
+    public static void printAdminMenu() {
+
+        final List<String> MENU = new ArrayList<String>(Arrays.asList("show all mentors", "exit\n"));
+
+        ListIterator<String> iterator = MENU.listIterator();
+
+        out.println("\nADMIN MENU: ");
+        while (iterator.hasNext()) {
+            out.println((iterator.nextIndex() + 1) + ". " + iterator.next());
+        }
+    }
+
 }
