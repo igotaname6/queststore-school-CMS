6e478e2205a5b503aedd4cf50ee60b7eded02ffd
Fesh <maciej.ukowski@gmail.com>
2017-09-28 15:17:47 +0200

    Add Login class with constuctors and getters

--- src/main/java/com/codecool_mjs/model/Login.java
+++ src/main/java/com/codecool_mjs/model/Login.java
@@ -1,4 +1,20 @@
 package com.codecool_mjs.model;
 
 public class Login {
+
+    private Integer id;
+    private String profession;
+
+    public Login(Integer id, String profession) {
+        this.id = id;
+        this.profession = profession;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
 }
