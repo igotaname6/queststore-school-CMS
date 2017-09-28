38f2682a9e5f752df250419c7605838bcd29ca4b
Fesh <maciej.ukowski@gmail.com>
2017-09-28 15:13:04 +0200

    Add LoginDao class with constuctor and getLoginData method

--- src/main/java/com/codecool_mjs/dataaccess/dao/LoginDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/LoginDao.java
@@ -1,4 +1,40 @@
 package com.codecool_mjs.dataaccess.dao;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+import com.codecool_mjs.model.Login;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class LoginDao {
+
+    Connection connection;
+
+    public LoginDao() throws SQLException {
+
+        this.connection = ConnectionProvider.getConnection();
+    }
+
+    public Login getLoginData(String email, String password) throws SQLException {
+
+        String query = "SELECT * FROM users WHERE email = '" + email + "' AND password = '" + password + "'";
+
+        Statement statement;
+        ResultSet result;
+        Integer id = null;
+        String profession = null;
+        Login login;
+
+        statement = this.connection.createStatement();
+        result = statement.executeQuery(query);
+
+        id = result.getInt("id");
+        profession = result.getString("profession");
+
+        login = new Login(id, profession);
+
+        return login;
+    }
 }
