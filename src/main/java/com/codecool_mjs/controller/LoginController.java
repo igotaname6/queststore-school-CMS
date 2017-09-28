d3b8676421c86f27b05a95184d54e0e3e70650b0
Fesh <maciej.ukowski@gmail.com>
2017-09-28 15:18:45 +0200

    Add LoginController with constuctor and login, getLoginData methods

--- src/main/java/com/codecool_mjs/controller/LoginController.java
+++ src/main/java/com/codecool_mjs/controller/LoginController.java
@@ -1,4 +1,70 @@
 package com.codecool_mjs.controller;
 
-public class LoginController {
+import com.codecool_mjs.View.LoginView;
+import com.codecool_mjs.dataaccess.dao.LoginDao;
+import com.codecool_mjs.model.Login;
+
+import java.sql.SQLException;
+
+public class LoginController implements Loginable{
+
+    LoginDao loginDao;
+
+    public LoginController() throws SQLException {
+
+        this.loginDao = new LoginDao();
+    }
+
+    public void startController() {
+
+        Login user;
+        Boolean isRunning = true;
+
+        LoginView.print("Welcome to CoinMasters 2.0");
+
+        while (isRunning) {
+            user = getLoginData();
+            login(user);
+        }
+    }
+
+    @Override
+    public Integer login(Login user) {
+
+        String profession;
+        Integer id;
+
+        id = user.getId();
+        profession = user.getProfession();
+
+        if (profession.equals("admin")) {
+            AdminMenuController adminMenuController = new AdminMenuController();
+            adminController.startController();
+
+        } else if (profession.equals("mentor")) {
+            System.out.println("Mentor Controller");
+//            MentorMenuController mentorMenuController = new MentorMenuController(id);
+//            mentorMenuController.startController();
+        } else if (profession.equals("codecooler")) {
+            System.out.println("Codecooler Controller");
+//            CodecoolerMenuController codecoolerMenuController = new CodecoolerMenuController(id);
+//            codecoolerMenuController.startController();
+        } else {
+            LoginView.print("Wrong email or password!");
+        }
+    }
+
+    private Login getLoginData() throws SQLException {
+
+        String email;
+        String password;
+        Login user;
+
+        email = LoginView.getEmail();
+        password = LoginView.getPassword();
+
+        user = this.loginDao.getLoginData(email, password);
+
+        return user;
+    }
 }
