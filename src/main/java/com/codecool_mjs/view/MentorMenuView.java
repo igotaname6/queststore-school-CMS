de097f6934cc80a74401e1fdb557d096b2f1eb2d
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-09-28 16:26:42 +0200

    Add printMentorMenu method

--- src/main/java/com/codecool_mjs/view/MentorMenuView.java
+++ src/main/java/com/codecool_mjs/view/MentorMenuView.java
@@ -1,4 +1,23 @@
 package com.codecool_mjs.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
+import static java.lang.System.out;
+
 public class MentorMenuView {
+
+    public static void printMentorMenu() {
+
+        final List<String> MENU = new ArrayList<String>(Arrays.asList("show all students", "log out\n"));
+
+        ListIterator<String> iterator = MENU.listIterator();
+
+        out.println("\nMENTOR MENU: ");
+        while (iterator.hasNext()) {
+            out.println((iterator.nextIndex() + 1) + ". " + iterator.next());
+        }
+    }
 }
