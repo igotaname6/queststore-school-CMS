6c99fa17945f4ad6acf535fed7fbb2cb19af2d9b
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-09-29 09:44:18 +0200

    refctor CodecoolerController name

--- src/main/java/com/codecool_mjs/controller/coodecoolerController/CodecoolerController.java
+++ src/main/java/com/codecool_mjs/controller/coodecoolerController/CodecoolerController.java
@@ -1,4 +1,4 @@
-package com.codecool_mjs.controller;
+package com.codecool_mjs.controller.coodecoolerController;
 
 import com.codecool_mjs.dataaccess.dao.CodecoolerDao;
 import com.codecool_mjs.dataaccess.dao.Dao;
