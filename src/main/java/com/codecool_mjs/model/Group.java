921f2b2d3c46a0a6d765cf53639b39df3429a5ed
Fesh <maciej.ukowski@gmail.com>
2017-10-09 11:42:37 +0200

    Remove lists from model

--- src/main/java/com/codecool_mjs/model/Group.java
+++ src/main/java/com/codecool_mjs/model/Group.java
@@ -5,40 +5,18 @@ import java.util.ArrayList;
 public class Group {
 
     private String name;
-    private ArrayList<Codecooler> members;
-    private ArrayList<Mentor> mentors;
 
     public Group(String name) {
+
         this.name = name;
-        this.members = new ArrayList<>();
-        this.mentors = new ArrayList<>();
+    }
+
+    public Group() {
+
+        this.name = null;
     }
 
     public String getName() {
         return this.name;
-
-    }
-    public ArrayList<Codecooler> getMembers() {
-        return this.members;
-    }
-
-    public ArrayList<Mentor> getMentors() {
-        return this.mentors;
-    }
-
-    public void addMentor(Mentor mentor) {
-        this.mentors.add(mentor);
-    }
-
-    public void addMember(Codecooler member) {
-        this.members.add(member);
-    }
-
-    public void removeMentor(Mentor mentor) {
-        this.mentors.remove(mentor);
-    }
-
-    public void removeMember(Codecooler member) {
-        this.members.remove(member);
     }
 }
