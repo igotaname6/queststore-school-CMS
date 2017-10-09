7784bbcc3cb85dbfa3eac1eca0f04f7dcd3c17c3
Fesh <maciej.ukowski@gmail.com>
2017-10-09 13:18:42 +0200

    Add default constuctor, change id declaration from int to Integer

--- src/main/java/com/codecool_mjs/model/Group.java
+++ src/main/java/com/codecool_mjs/model/Group.java
@@ -4,19 +4,34 @@ import java.util.ArrayList;
 
 public class Group {
 
+    private Integer id;
     private String name;
-
-    public Group(String name) {
-
-        this.name = name;
-    }
 
     public Group() {
 
+        this.id = null;
         this.name = null;
     }
 
+    public Group(Integer id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getName() {
-        return this.name;
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
