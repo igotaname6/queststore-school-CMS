937bcbe76e26f95bea5059187d33f630e214fea4
Fesh <maciej.ukowski@gmail.com>
2017-10-09 11:26:07 +0200

    Add groupId to constuctor

--- src/main/java/com/codecool_mjs/model/Codecooler.java
+++ src/main/java/com/codecool_mjs/model/Codecooler.java
@@ -2,15 +2,22 @@ package com.codecool_mjs.model;
 
 public class Codecooler extends User {
 
+    private Integer groupId;
     private Wallet wallet;
 
-    public Codecooler(Integer id, String name, String surname, String email, String password, Wallet wallet) {
+
+    public Codecooler(Integer id, String name, String surname, String email, String password,
+                      Integer groupId, Wallet wallet) {
+
         super(id, name, surname, email, password);
+        this.groupId = groupId;
         this.wallet = wallet;
     }
 
     public Codecooler() {
+
         super();
+        this.groupId = null;
         this.wallet = null;
     }
 
@@ -20,5 +27,13 @@ public class Codecooler extends User {
 
     public void setWallet(Wallet wallet) {
         this.wallet = wallet;
+    }
+
+    public Integer getGroupId() {
+        return this.groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
     }
 }
