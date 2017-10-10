0b9e4de3e59e34bddec8536c3faf38e980a98b18
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-10-10 10:22:26 +0200

    change generic type from User to Codecooler

--- src/main/java/com/codecool_mjs/dataaccess/dao/CodecoolerDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/CodecoolerDao.java
@@ -1,21 +1,20 @@
 package com.codecool_mjs.dataaccess.dao;
 
 import com.codecool_mjs.model.Codecooler;
-import com.codecool_mjs.model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class CodecoolerDao extends Dao<User> {
+public class CodecoolerDao extends Dao<Codecooler> {
 
     private static String PROFESSION = "codecooler";
 
     @Override
-    User createObject(ResultSet results) throws SQLException {
+    Codecooler createObject(ResultSet results) throws SQLException {
 
-        User codecooler = null;
+        Codecooler codecooler = null;
 
         Integer id = results.getInt("id");
         String name = results.getString("name");
@@ -29,11 +28,11 @@ public class CodecoolerDao extends Dao<User> {
     }
 
     @Override
-    Integer executeDeletion(User user) throws SQLException {
+    Integer executeDeletion(Codecooler codecooler) throws SQLException {
         Connection conn = getConnection();
 
         PreparedStatement statement = conn.prepareStatement(getDeletionStatement());
-        statement.setInt(1, user.getId());
+        statement.setInt(1, codecooler.getId());
 
 
         Integer rowAffected = statement.executeUpdate();
@@ -41,31 +40,46 @@ public class CodecoolerDao extends Dao<User> {
     }
 
     @Override
-    String getQueryGetAll() {
+    String getQueryForGetAll() {
 
         String query = "SELECT * FROM users WHERE profession = 'codecooler'";
 
         return query;
     }
 
-    String getQuerySearchBy(String category, String arg) {
+    String getQueryForSearchBy(String category, String arg) {
 
         String query = String.format("SELECT * FROM artifacts WHERE %s LIKE '%s' AND profession = 'codelooler'", category, arg);
 
         return query;
     }
 
-    Integer executeInsertation(User user) throws SQLException {
+    Integer executeInsertation(Codecooler codecooler) throws SQLException {
         Connection conn = getConnection();
 
         PreparedStatement statement1 = conn.prepareStatement(getInsertationStatement());
-        statement1.setString(1, user.getName());
-        statement1.setString(2, user.getSurname());
-        statement1.setString(3, user.getEmail());
-        statement1.setString(4, user.getPassword());
+        statement1.setString(1, codecooler.getName());
+        statement1.setString(2, codecooler.getSurname());
+        statement1.setString(3, codecooler.getEmail());
+        statement1.setString(4, codecooler.getPassword());
         statement1.setString(5, PROFESSION);
 
         Integer rowAffected = statement1.executeUpdate();
+        return rowAffected;
+    }
+
+    protected Integer executeUpdateStatements(Codecooler codecooler) throws SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement statement = connection.
+                prepareStatement(getUpdateStatement());
+
+        statement.setString(1, codecooler.getName());
+        statement.setString(2, codecooler.getSurname());
+        statement.setString(3, codecooler.getEmail());
+        statement.setString(4, codecooler.getPassword());
+
+        Integer rowAffected =statement.executeUpdate();
         return rowAffected;
     }
 
@@ -78,6 +92,13 @@ public class CodecoolerDao extends Dao<User> {
     private String getDeletionStatement() {
         return "DELETE FROM users WHERE id = ?;";
     }
+
+    private String getUpdateStatement(){
+        return "UPDATE users " +
+                "set name = ?, surname = ?, email = ?, password = ?";
+    }
+
+
 }
 
 
