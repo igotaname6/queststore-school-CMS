8857c5def8f2ab06dca097b765dd46fa80a7e914
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-10-10 11:14:13 +0200

    create UserDao class with methods to User integration with DB

---
+++ src/main/java/com/codecool_mjs/dataaccess/dao/UserDao.java
@@ -0,0 +1,78 @@
+package com.codecool_mjs.dataaccess.dao;
+
+import com.codecool_mjs.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class UserDao<T extends User> extends Dao <T> {
+
+    @Override
+    Integer executeDeletion(T t) throws SQLException {
+        Connection conn = getConnection();
+
+        PreparedStatement statement = conn.prepareStatement(getDeletionStatement());
+        statement.setInt(1, t.getId());
+
+
+        Integer rowAffected = statement.executeUpdate();
+        return rowAffected;
+    }
+
+    String getQueryForSearchBy(String category, String arg) {
+
+        String query = String.format("SELECT * FROM artifacts WHERE %s LIKE '%s' AND profession = '%s'", category, arg, getProfession());
+
+        return query;
+    }
+
+    Integer executeInsertation(T t) throws SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement statement = connection.prepareStatement(getInsertationStatement());
+        statement.setString(1, t.getName());
+        statement.setString(2, t.getSurname());
+        statement.setString(3, t.getEmail());
+        statement.setString(4, t.getPassword());
+        statement.setString(5, getProfession());
+
+        Integer rowAffected = statement.executeUpdate();
+        return rowAffected;
+    }
+
+    protected Integer executeUpdateStatements(T t) throws SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement statement = connection.
+                prepareStatement(getUpdateStatement());
+
+        statement.setString(1, t.getName());
+        statement.setString(2, t.getSurname());
+        statement.setString(3, t.getEmail());
+        statement.setString(4, t.getPassword());
+        statement.setInt(5, t.getId());
+
+        Integer rowAffected = statement.executeUpdate();
+        return rowAffected;
+    }
+
+    private String getInsertationStatement() {
+        return "INSERT INTO users (" +
+                "name, surname, email, password, profession)" +
+                "VALUES (?, ?, ?, ?, ?)";
+    }
+
+    private String getDeletionStatement() {
+        return "DELETE FROM users WHERE id = ?;";
+    }
+
+    private String getUpdateStatement() {
+        return "UPDATE users " +
+                "set name = ?, surname = ?, email = ?, password = ?" +
+                "WHERE id = ?";
+    }
+
+    abstract String getProfession();
+
+}
