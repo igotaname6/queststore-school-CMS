a4829db925f188e7d0d96ec194adb3f60d6f8a38
Kacper Zelichowski <kafejek@gmail.com>
2017-10-11 11:17:37 +0200

    Create test method for Artifact, implement test method for Admin constructor

--- src/test/java/com/codecool_mjs/model/AdminTest.java
+++ src/test/java/com/codecool_mjs/model/AdminTest.java
@@ -1,7 +1,19 @@
 package com.codecool_mjs.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdminTest {
 
+    @Test
+    public void testAdminConstructorForCorrectValues() {
+        Admin admin = new Admin(1, "Test", "Name", "dont@pay.at", "tention");
+        String expectedPassword = "tention";
+        Integer expectedID = 1;
+        String expectedEmail = "dont@pay.at";
+        assertEquals(expectedPassword, admin.getPassword());
+        assertEquals(expectedID, admin.getId());
+        assertEquals(expectedEmail, admin.getEmail());
+    }
 }
--- src/test/java/com/codecool_mjs/model/ArtifactTest.java
+++ src/test/java/com/codecool_mjs/model/ArtifactTest.java
@@ -1,7 +1,20 @@
 package com.codecool_mjs.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArtifactTest {
+
+    @Test
+    public void testArtifactEmptyConstructor() {
+        Artifact artifact = new Artifact();
+        assertSame(new Artifact(), artifact);
+    }
+
+    @Test
+    public void testArtifactParametrizedConstructor() {
+
+    }
 
 }
