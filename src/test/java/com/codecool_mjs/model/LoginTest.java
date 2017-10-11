03c2f4786d73972590a4f1de46ca9799b21c9734
Kacper Zelichowski <kafejek@gmail.com>
2017-10-11 14:40:02 +0200

    Implement tests for Login model

--- src/test/java/com/codecool_mjs/model/LoginTest.java
+++ src/test/java/com/codecool_mjs/model/LoginTest.java
@@ -1,7 +1,34 @@
 package com.codecool_mjs.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginTest {
 
+    Login login;
+
+    @BeforeEach
+    public void setup() {
+
+        this.login = new Login(1, "test");
+    }
+
+    @Test
+    public void testConstructorForNegativeID() {
+        assertThrows(IllegalArgumentException.class, () -> {
+           Login login = new Login(-1, "test");
+        });
+    }
+
+    @Test
+    public void testGetID() {
+        assertEquals(1, (int) login.getId());
+    }
+
+    @Test
+    public void testGetProffession() {
+        assertEquals("test", login.getProfession());
+    }
 }
