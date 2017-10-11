d6c6219d6c5d5afa11265579d6d90386ec55f899
smoczis <romanowski.mateusz93@gmail.com>
2017-10-11 14:56:01 +0200

    Tests connection to database

--- src/test/java/com/codecool_mjs/dataaccess/ConnectionProviderTest.java
+++ src/test/java/com/codecool_mjs/dataaccess/ConnectionProviderTest.java
@@ -1,9 +1,17 @@
 package com.codecool_mjs.dataaccess;
 
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConnectionProviderTest {
 
-    
+    @Test
+    public void testCreateConnection() {
+
+        assertTrue(ConnectionProvider.getConnection() != null);
+    }
 
 }
