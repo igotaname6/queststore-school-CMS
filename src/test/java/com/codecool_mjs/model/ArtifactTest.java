656ccff5b9c577bfd115e47561682ba42f88179f
Kacper Zelichowski <kafejek@gmail.com>
2017-10-11 12:54:18 +0200

    Create test methods for CodecoolerTest, fix issues in artifact and artifacttest

--- src/test/java/com/codecool_mjs/model/ArtifactTest.java
+++ src/test/java/com/codecool_mjs/model/ArtifactTest.java
@@ -9,12 +9,61 @@ class ArtifactTest {
     @Test
     public void testArtifactEmptyConstructor() {
         Artifact artifact = new Artifact();
-        assertSame(new Artifact(), artifact);
+        assertNull(artifact.getDescription());
     }
 
     @Test
     public void testArtifactParametrizedConstructor() {
+        Artifact artifact = new Artifact(1, "test", "test", 1, true);
+        assertNotNull(artifact.getIsGroup());
+    }
 
+    @Test
+    public void testArtifactParametrizedConstructor2() {
+        Artifact artifact = new Artifact(1, "test", "test", 1, false, true);
+        assertNotNull(artifact.getIsUsed());
+    }
+
+    @Test
+    public void testArtifactSetGetID() {
+        Artifact artifact = new Artifact();
+        artifact.setId(1);
+        assertEquals(1, (int)artifact.getId());
+    }
+
+    @Test
+    public void testArtifactSetGetName() {
+        Artifact artifact = new Artifact();
+        artifact.setName("test");
+        assertEquals("test", artifact.getName());
+    }
+
+    @Test
+    public void testArtifactSetGetDescription() {
+        Artifact artifact = new Artifact();
+        artifact.setDescription("test");
+        assertEquals("test", artifact.getDescription());
+    }
+
+    @Test
+    public void testArtifactSetGetCost() {
+        Artifact artifact = new Artifact();
+        artifact.setCost(1);
+        assertEquals(1, (int)artifact.getCost());
+    }
+
+    @Test
+    public void testArtifactSetGetIsUsed() {
+        Artifact artifact = new Artifact();
+        artifact.setIsUsed(true);
+        assertNotNull(artifact.getIsUsed());
+    }
+
+    @Test
+    public void testArtifactSetGetIsGroup() {
+        Artifact artifact = new Artifact();
+        artifact.setIsGroup(true);
+        assertNotNull(artifact.getIsGroup());
     }
 
 }
--- src/test/java/com/codecool_mjs/model/CodecoolerTest.java
+++ src/test/java/com/codecool_mjs/model/CodecoolerTest.java
@@ -1,7 +1,34 @@
 package com.codecool_mjs.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CodecoolerTest {
+
+    @Test
+    public void testCodecoolerEmptyConstructor() {
+        Codecooler codecooler = new Codecooler();
+        assertNull(codecooler.getWallet());
+    }
+
+    @Test
+    public void testCodecoolerParametrizedConstructor() {
+        Codecooler codecooler = new Codecooler(1, "test", "test", "test", "test");
+        assertNotNull(codecooler.getId());
+    }
+
+//    @Test
+//    public void testCodecoolerParametrizedConstructor2() {
+//        Codecooler codecooler = new Codecooler();
+//        assertNull(codecooler.getWallet());
+//    }
+
+    @Test
+    public void testCodecoolerSetGetID() {
+        Codecooler codecooler = new Codecooler();
+        codecooler.setGroupId(1);
+        assertEquals(1, (int)codecooler.getGroupId());
+    }
 
 }
