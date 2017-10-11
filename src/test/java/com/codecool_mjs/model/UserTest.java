928a84dc87035ef4c2423874dd0eb4f3c0593e06
Michał Urban <michalurban23@gmail.com>
2017-10-11 13:51:04 +0200

    Add tests to User and Wallet setter and getter methods

--- src/test/java/com/codecool_mjs/model/UserTest.java
+++ src/test/java/com/codecool_mjs/model/UserTest.java
@@ -1,7 +1,65 @@
 package com.codecool_mjs.model;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+
+        User user = new Codecooler();
+
+    @Test
+    void testSetterAndGetterName() {
+
+        String testName = "Stefan";
+
+        user.setName(testName);
+        String name = user.getName();
+
+        assertEquals(testName, name);
+    }
+
+    @Test
+    void testSetterAndGetterSurname() {
+
+        String testSurname = "Batory";
+
+        user.setSurname(testSurname);
+        String surname = user.getSurname();
+
+        assertEquals(testSurname, surname);
+    }
+
+    @Test
+    void testSetterAndGetterEmail() {
+
+        String testEmail = "test@test.test";
+
+        user.setEmail(testEmail);
+        String email = user.getEmail();
+
+        assertEquals(testEmail, email);
+    }
+
+    @Test
+    void testSetterAndGetterID() {
+
+        Integer testId = 111;
+
+        user.setId(testId);
+        Integer id = user.getId();
+
+        assertEquals(testId, id);
+    }
+
+    @Test
+    void testSetterAndGetterPassword() {
+
+        String testPassword = "abc123";
+
+        user.setPassword(testPassword);
+        String password = user.getPassword();
+
+        assertEquals(testPassword, password);
+    }
 
 }
