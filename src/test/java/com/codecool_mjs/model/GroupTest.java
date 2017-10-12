1f18279382061218f46f6e9abab9ccc2cfcb197d
blazpie <blazej.pierzak@gmail.com>
2017-10-12 15:14:22 +0200

    Add tests to group model class

--- src/test/java/com/codecool_mjs/model/GroupTest.java
+++ src/test/java/com/codecool_mjs/model/GroupTest.java
@@ -1,7 +1,60 @@
 package com.codecool_mjs.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupTest {
+    @Test
+    void testGetId() {
+        Group testGroup = new Group();
+        testGroup.setId(12);
+        assertEquals(12, (int) testGroup.getId());
+    }
+
+    @Test
+    void testSetId() {
+        Group testGroup = new Group();
+        testGroup.setId(12);
+        assertEquals(12, (int) testGroup.getId());
+    }
+
+    @Test
+    void testGetName() {
+        Group testGroup = new Group();
+        testGroup.setName("Janusz");
+        assertEquals("Janusz", testGroup.getName());
+    }
+
+    @Test
+    void testSetName() {
+        Group testGroup = new Group();
+        testGroup.setName("Janusz");
+        assertEquals("Janusz", testGroup.getName());
+    }
+
+    @Test
+    void testEmptyConstructorSetIdNull() {
+        Group testGroup = new Group();
+        assertNull(testGroup.getId());
+    }
+
+    @Test
+    void testEmptyConstructorSetNameNull() {
+        Group testGroup = new Group();
+        assertNull(testGroup.getName());
+    }
+
+    @Test
+    void testParametrizedConstructorSetId() {
+        Group testGroup = new Group(12, "Janusz");
+        assertEquals(12, (int) testGroup.getId());
+    }
+
+    @Test
+    void testParametrizedConstructorSetName() {
+        Group testGroup = new Group(12, "Janusz");
+        assertEquals("Janusz", testGroup.getName());
+    }
 
 }
