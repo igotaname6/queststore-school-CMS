8147bc08844eab61826cb32929e63be14a44758f
Michał Urban <michalurban23@gmail.com>
2017-10-12 13:34:56 +0200

    Remove redundant imports

--- src/test/java/com/codecool_mjs/view/AdminMenuViewTest.java
+++ src/test/java/com/codecool_mjs/view/AdminMenuViewTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
