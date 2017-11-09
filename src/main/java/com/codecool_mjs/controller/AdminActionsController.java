faf17bcb24f2300163650493c9e326a2738026b0
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-11-09 11:24:00 +0100

    implements showAllMentors method

--- src/main/java/com/codecool_mjs/controller/AdminActionsController.java
+++ src/main/java/com/codecool_mjs/controller/AdminActionsController.java
@@ -1,29 +1,51 @@
 package com.codecool_mjs.controller;
 
 import com.codecool_mjs.dataaccess.dao.DaoException;
+import com.codecool_mjs.model.Admin;
 import com.codecool_mjs.model.Mentor;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.thymeleaf.TemplateEngine;
 
 import java.io.IOException;
-import java.io.StringWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AdminActionsController implements HttpHandler{
 
+    private TemplatesEngine templateEngine;
+    private Admin loggedUser;
 
+
+    public AdminActionsController(){
+        this.templateEngine = new TemplatesEngine();
+    }
+
+    public void setLoggedUser(Admin loggedUser) {
+        this.loggedUser = loggedUser;
+    }
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
     }
 
-    public void showAllMentors() throws DaoException {
+    public String showAllMentors() throws DaoException {
+        //temporary example of logged user. To remove when sessions will be implemented
+        setLoggedUser(new Admin(15,"Janusz", "Kowalski", "j.k@cc.pl", "typoweHasło"));
+
+        Map<String, Object> variables = new HashMap<>();
+
         MentorController mentorController = new MentorController();
         List<Mentor> allMentors = mentorController.getAllMentors();
 
+        variables.put("user", loggedUser);
+        variables.put("mentorsList", allMentors);
 
+        templateEngine.setVariables(variables);
 
-
+        String page = templateEngine.ProcessTemplateToPage("admin-show-mentors");
+        return page;
     }
 }
