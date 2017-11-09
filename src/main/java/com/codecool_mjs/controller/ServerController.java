c6711cfdd8f4b938b96f1cfe8011e5a49c69b685
Fesh <maciej.ukowski@gmail.com>
2017-11-09 11:47:45 +0100

    Add Server controller

--- src/main/java/com/codecool_mjs/controller/ServerController.java
+++ src/main/java/com/codecool_mjs/controller/ServerController.java
@@ -1,4 +1,18 @@
 package com.codecool_mjs.controller;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class ServerController {
+
+    public void createAndStartSever() throws IOException {
+
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            server.createContext("/admin", new AdminActionsController());
+            server.createContext("static", new StaticController());
+            server.setExecutor(null);
+            server.start();
+    }
 }
