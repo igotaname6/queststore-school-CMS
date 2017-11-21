169dbac30f351dc080c29828019e29d9441bc1fe
Fesh <maciej.ukowski@gmail.com>
2017-11-21 13:27:20 +0100

    Add new contexts

--- src/main/java/com/codecool_mjs/severProvider/ServerController.java
+++ src/main/java/com/codecool_mjs/severProvider/ServerController.java
@@ -3,6 +3,8 @@ package com.codecool_mjs.severProvider;
 import com.codecool_mjs.controller.webAccessController.adminActionsController.AdminHomeController;
 import com.codecool_mjs.controller.webAccessController.adminActionsController.ShowMentorsActions;
 import com.codecool_mjs.controller.webAccessController.logIn.HomeController;
+import com.codecool_mjs.controller.webAccessController.mentorActionController.MentorHomeController;
+import com.codecool_mjs.controller.webAccessController.studentActionController.StudentHomeController;
 import com.codecool_mjs.controller.webAccessController.utilController.StaticController;
 import com.sun.net.httpserver.HttpServer;
 
@@ -15,8 +17,10 @@ public class ServerController {
 
             HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
             server.createContext("/home", new HomeController());
-            server.createContext("/admin-show-mentors", new ShowMentorsActions());
             server.createContext("/admin-home", new AdminHomeController());
+            server.createContext("/admin-show-mentors", new ShowMentorsActions());
+            server.createContext("/mentor-home", new MentorHomeController());
+            server.createContext("/student-home", new StudentHomeController());
             server.createContext("/static", new StaticController());
             server.setExecutor(null);
             server.start();
