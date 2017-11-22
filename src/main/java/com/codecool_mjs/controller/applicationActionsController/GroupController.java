842b173baeb7387273c2f9e06b6e907e37cee948
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-22 14:42:03 +0100

    Add setDao method in GroupController

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/GroupController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/GroupController.java
@@ -1,5 +1,8 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+import com.codecool_mjs.dataaccess.dao.DaoException;
+import com.codecool_mjs.dataaccess.dao.GroupDao;
 import com.codecool_mjs.dataaccess.dao.IDao;
 import com.codecool_mjs.model.Group;
 
@@ -8,5 +11,15 @@ public class GroupController {
     private IDao<Group> dao;
 
     public GroupController(){
+        setDao();
+    }
+
+    private void setDao(){
+        try{
+            dao = new GroupDao();
+            ConnectionProvider.getInstance().connectionRequest(dao);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
     }
 }
