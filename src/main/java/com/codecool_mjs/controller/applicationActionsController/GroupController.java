67af86f14fe749ac877d9f74ba838969b8bf77fd
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-22 14:40:38 +0100

    Add GroupController costructor

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/GroupController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/GroupController.java
@@ -1,4 +1,12 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.dao.IDao;
+import com.codecool_mjs.model.Group;
+
 public class GroupController {
+
+    private IDao<Group> dao;
+
+    public GroupController(){
+    }
 }
