2c153e61d7839529951cf3eed1779365dd020223
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-23 14:35:34 +0100

    refactor login method

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/SessionController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/SessionController.java
@@ -1,7 +1,10 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.dao.DaoException;
 import com.codecool_mjs.dataaccess.dao.LogInDao;
 import com.codecool_mjs.model.User;
+
+import java.util.UUID;
 
 public class SessionController{
 
@@ -12,9 +15,20 @@ public class SessionController{
         dao = new LogInDao();
     }
 
-    public void logIn(String userName, String password){
+    public void logIn(String email, String password) throws DaoException{
+        User user;
 
+        user = dao.checkLogin(email, password);
+        if(user != null){
+            String uuid = createSessionUUID();
+        }
 
     }
+
+    private String createSessionUUID(){
+        return UUID.randomUUID().toString();
+    }
+
+
 
 }
