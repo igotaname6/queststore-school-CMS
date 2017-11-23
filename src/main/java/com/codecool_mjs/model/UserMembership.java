6f2fc74e0e87ace4cb719a4ca06467dd6cfb806e
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-23 14:41:57 +0100

    Add UserMembership class

---
+++ src/main/java/com/codecool_mjs/model/UserMembership.java
@@ -0,0 +1,23 @@
+package com.codecool_mjs.model;
+
+import java.util.List;
+
+public class UserMembership {
+
+    private List<Mentor> mentors;
+    private List<Codecooler> codecoolers;
+
+    public List<Mentor> getMentors() {
+        return mentors;
+    }
+
+    public List<Codecooler> getCodecoolers() {
+        return codecoolers;
+    }
+
+    public UserMembership(List<Mentor> mentors, List<Codecooler> codecoolers) {
+
+        this.mentors = mentors;
+        this.codecoolers = codecoolers;
+    }
+}
