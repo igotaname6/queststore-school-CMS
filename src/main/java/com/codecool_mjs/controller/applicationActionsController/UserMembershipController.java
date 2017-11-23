93ae419497837ae186ec0993c1a9384a15f6e0df
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-23 17:15:01 +0100

    Add addMemebership method

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/UserMembershipController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/UserMembershipController.java
@@ -4,11 +4,16 @@ import com.codecool_mjs.dataaccess.ConnectionProvider;
 import com.codecool_mjs.dataaccess.dao.DaoException;
 import com.codecool_mjs.dataaccess.dao.IDao;
 import com.codecool_mjs.dataaccess.dao.UserMembershipDao;
+import com.codecool_mjs.model.Group;
+import com.codecool_mjs.model.User;
 import com.codecool_mjs.model.UserMembership;
+
+import java.util.List;
+
 
 public class UserMembershipController {
     private IDao<UserMembership> dao;
-    private static MentorController instance = null;
+    private static UserMembershipController instance = null;
 
     public UserMembershipController(){
         setDao();
@@ -23,10 +28,22 @@ public class UserMembershipController {
         }
     }
 
-    public static MentorController getInstance() {
+    public static UserMembershipController getInstance() {
         if(instance==null){
-            instance = new MentorController();
+            instance = new UserMembershipController();
         }
         return instance;
+    }
+
+    public void addMembership(List<Integer> usersId) throws DaoException {
+
+        Group group = GroupController.getInstance().getLastGroup();
+
+        for (Integer id : usersId) {
+            User user = MentorController.getInstance().getMentorById(id);
+            UserMembership um = new UserMembership(user, group);
+            this.dao.insert(um);
+            //coś się dodaje ale nie do końca to, co trzeba, zrobim jutro resztę
+        }
     }
 }
