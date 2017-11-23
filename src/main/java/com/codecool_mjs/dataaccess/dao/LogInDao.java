29f044059a0d4cd9fc679a677e3cd34d1dc4e20e
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-23 14:36:07 +0100

    implements checkLogin Method in logInDao

--- src/main/java/com/codecool_mjs/dataaccess/dao/LogInDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/LogInDao.java
@@ -15,42 +15,91 @@ public class LogInDao {
     private static final String DRIVER_CLASS = "org.sqlite.JDBC";
 
 
-    public User getUserById(int id) throws DaoException {
-        String query = "SELECT * from users WHERE id = ?";
+    public User checkLogin(String email, String password) throws DaoException {
+        String query = "SELECT * FROM users WHERE email = ? AND password = ?";
         try {
             Class.forName(DRIVER_CLASS);
             this.connection = DriverManager.getConnection(URL);
 
             PreparedStatement preStatement = connection.prepareStatement(query);
-            preStatement.setInt(1, id);
+            preStatement.setString(1, email);
+            preStatement.setString(2, password);
             ResultSet rs = preStatement.executeQuery();
 
             User user;
 
-            if(rs.isClosed()){
+            if (rs.isClosed()) {
                 return null;
             }
 
-            String name = rs.getString("name");
-            String surname = rs.getString("surname");
-            String email = rs.getString("email");
-            String password = rs.getString("password");
-            String profession = rs.getString("profession");
+            user = createUser(rs, email, password);
 
-            switch(profession){
-                case "codecooler": user = new Codecooler(id, name, surname, email, password);
-                    break;
-                case "mentor": user = new Mentor(id, name, surname, email, password);
-                    break;
-                case "admin": user = new Admin(id, name, surname, email, password);
-                    break;
-                default: user = null;
-                    break;
-            }
             connection.close();
             return user;
         } catch (SQLException | ClassNotFoundException e) {
             throw new DaoException("Exception in loginDao", e);
+        }
+    }
+
+    private User createUser(ResultSet rs, String email, String password) throws SQLException {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String surname = rs.getString("surname");
+        String profession = rs.getString("profession");
+
+        User user = null;
+
+        if (profession.equals("codecooler")) {
+            user = new Codecooler(id, name, surname, email, password);
+        } else if (profession.equals("mentor")) {
+            user = new Mentor(id, name, surname, email, password);
+        } else if (profession.equals("admin")) {
+            user = new Admin(id, name, surname, email, password);
+        }
+        return user;
+    }
+
+    public boolean addSession(String uuid, int userId) throws DaoException {
+        String query = "INSERT INTO session (session_id, user_id)" +
+                "VALUES(? , ?)";
+        try {
+            Class.forName(DRIVER_CLASS);
+            this.connection = DriverManager.getConnection(URL);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, uuid);
+            preparedStatement.setInt(2, userId);
+            int result = preparedStatement.executeUpdate();
+            if (result == 1) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new DaoException("Exception in addSession", e);
+
+        }
+    }
+
+    public boolean checkSessionStatus(String session_id) throws DaoException {
+        String query = "SELECT * FROM sessions WHERE session_id = ?";
+
+        try {
+            Class.forName(DRIVER_CLASS);
+            this.connection = DriverManager.getConnection(URL);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,  session_id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if(rs.isClosed()){
+                return false;
+            } else {
+                return true;
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new DaoException("exception in checkSessionStatus", e);
         }
     }
 }
