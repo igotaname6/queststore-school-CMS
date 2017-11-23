76db37d49e6a2ffe3970ab588592a84ee128bb88
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-23 10:54:20 +0100

    create sessionController with default fields

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/SessionController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/SessionController.java
@@ -1,9 +1,20 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
-public class LoginController {
+import com.codecool_mjs.dataaccess.dao.LogInDao;
+import com.codecool_mjs.model.User;
+
+public class SessionController{
+
+    private LogInDao dao;
+    private User loggedUser;
+
+    public SessionController(){
+        dao = new LogInDao();
+    }
+
+    public void logIn(String userName, String password){
 
 
-
-
+    }
 
 }
