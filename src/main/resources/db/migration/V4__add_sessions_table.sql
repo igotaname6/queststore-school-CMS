7483c6dcf3323dd925aee99cd74ae482d3bc8a52
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-23 14:36:54 +0100

    reneme migration v4

--- src/main/resources/db/migration/V4__add_sessions_table.sql
+++ src/main/resources/db/migration/V4__add_sessions_table.sql
@@ -1,8 +1,4 @@
-
-
 CREATE TABLE sessions(
   'uuid' text PRIMARY KEY NOT NULL,
   'account_id' INTEGER NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES users(id),
-
-);
+    FOREIGN KEY (account_id) REFERENCES users(id));
