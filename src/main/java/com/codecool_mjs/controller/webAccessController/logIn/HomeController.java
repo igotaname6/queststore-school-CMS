4995e80e712e059562f89dc76679db207355a1e0
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-26 22:24:16 +0100

    Refactor handle method

--- src/main/java/com/codecool_mjs/controller/webAccessController/logIn/HomeController.java
+++ src/main/java/com/codecool_mjs/controller/webAccessController/logIn/HomeController.java
@@ -1,47 +1,57 @@
 package com.codecool_mjs.controller.webAccessController.logIn;
 
+import com.codecool_mjs.controller.applicationActionsController.SessionController;
+import com.codecool_mjs.dataaccess.dao.DaoException;
+import com.codecool_mjs.model.User;
 import com.codecool_mjs.view.webView.TemplatesProcessor;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class HomeController implements HttpHandler {
 
     private TemplatesProcessor templatesProcessor;
+    private SessionController sessionController;
 
     public HomeController() {
         this.templatesProcessor = new TemplatesProcessor();
+        this.sessionController = new SessionController();
     }
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        String responseBody = templatesProcessor.ProcessTemplateToPage("index");
+        Boolean isNewSession = sessionController.isNewSession(httpExchange);
+        String method = httpExchange.getRequestMethod();
 
-        httpExchange.sendResponseHeaders(200, responseBody.getBytes().length);
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(responseBody.getBytes());
-        os.close();
-    }
+        if (isNewSession && method.equals("GET")) {
+            String responseBody = templatesProcessor.ProcessTemplateToPage("index");
 
-    private Map<String, String> parseForm(HttpExchange httpExchange) throws IOException {
-
-        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
-        BufferedReader br = new BufferedReader(isr);
-        String formData = br.readLine();
-
-        String[] pairsArray = formData.split("&");
-
-        Map<String, String> formMap = new HashMap<>();
-        for(String pair: pairsArray){
-            String[] keyValue = pair.split("=");
-            formMap.put(keyValue[0], keyValue[1]);
+            httpExchange.sendResponseHeaders(200, responseBody.getBytes().length);
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(responseBody.getBytes());
+            os.close();
+        } else if (isNewSession && method.equals("POST")) {
+            try {
+                User user = sessionController.logIn(httpExchange);
+            } catch (DaoException e) {
+                e.printStackTrace();
+            }
+        } else {
+            String userRole = sessionController.getLoggedUser().getProfession();
+            if (userRole.equals("Mentor")) {
+                httpExchange.getResponseHeaders().add("Location", "/mentor-home");
+                httpExchange.sendResponseHeaders(302, -1);
+            } else if (userRole.equals("Codecooler")) {
+                httpExchange.getResponseHeaders().add("Location", "/codecooler-home");
+                httpExchange.sendResponseHeaders(302, -1);
+            } else if (userRole.equals("Admin")) {
+                httpExchange.getResponseHeaders().add("Location", "/admin-home");
+                httpExchange.sendResponseHeaders(302, -1);
+            }
         }
-        return formMap;
     }
-
-
 }
+
+
