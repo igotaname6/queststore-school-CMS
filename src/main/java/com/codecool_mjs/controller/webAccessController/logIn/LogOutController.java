4c4098a9e5b4cd5ff46a02009799c3b8eb0c5cca
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-27 18:13:12 +0100

    Create LogOutController class which implements HttpHandler

---
+++ src/main/java/com/codecool_mjs/controller/webAccessController/logIn/LogOutController.java
@@ -0,0 +1,14 @@
+package com.codecool_mjs.controller.webAccessController.logIn;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class LogOutController implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
