c9377dd6142e96653e22f5142a75de6b1758ba49
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-27 19:25:07 +0100

    Make QuestController singleton

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/QuestController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/QuestController.java
@@ -1,4 +1,38 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+import com.codecool_mjs.dataaccess.dao.Dao;
+import com.codecool_mjs.dataaccess.dao.DaoException;
+import com.codecool_mjs.dataaccess.dao.QuestDao;
+import com.codecool_mjs.model.Quest;
+
+import java.util.List;
+import java.util.Map;
+
 public class QuestController {
+
+    private Dao<Quest> dao;
+    private static QuestController instance = null;
+
+    public QuestController() {
+        setDao();
+    }
+
+    private void setDao() {
+        try {
+            dao = new QuestDao();
+            ConnectionProvider.getInstance().connectionRequest(dao);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static QuestController getInstance() {
+        if (instance == null) {
+            instance = new QuestController();
+        }
+        return instance;
+    }
+
+
 }
