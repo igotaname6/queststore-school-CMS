98c159d0190899fcca6cd67be8b958eb86086c31
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-27 19:51:19 +0100

    Implement handle method in LogOutController

--- src/main/java/com/codecool_mjs/controller/webAccessController/logIn/LogOutController.java
+++ src/main/java/com/codecool_mjs/controller/webAccessController/logIn/LogOutController.java
@@ -1,5 +1,7 @@
 package com.codecool_mjs.controller.webAccessController.logIn;
 
+import com.codecool_mjs.controller.applicationActionsController.SessionController;
+import com.codecool_mjs.dataaccess.dao.DaoException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -7,8 +9,20 @@ import java.io.IOException;
 
 public class LogOutController implements HttpHandler {
 
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    private SessionController sessionController;
 
+    public LogOutController() {
+        this.sessionController = new SessionController();
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        try {
+            sessionController.logOut(httpExchange);
+            httpExchange.getResponseHeaders().add("Location", "/home");
+            httpExchange.sendResponseHeaders(302, -1);
+        } catch (DaoException e) {
+            httpExchange.sendResponseHeaders(503, -1);
+        }
     }
 }
