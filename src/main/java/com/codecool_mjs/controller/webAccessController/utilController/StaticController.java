540f2cb7a5020f71ec31aeb36c22da68d028fdc9
Fesh <maciej.ukowski@gmail.com>
2017-11-27 17:27:41 +0100

    Delete redundant print

--- src/main/java/com/codecool_mjs/controller/webAccessController/utilController/StaticController.java
+++ src/main/java/com/codecool_mjs/controller/webAccessController/utilController/StaticController.java
@@ -22,7 +22,6 @@ public class StaticController implements HttpHandler {
 
         ClassLoader classLoader = getClass().getClassLoader();
         URL fileURL = classLoader.getResource(root + path);
-        System.out.println(root+ path);
 
         OutputStream os = httpExchange.getResponseBody();
 
