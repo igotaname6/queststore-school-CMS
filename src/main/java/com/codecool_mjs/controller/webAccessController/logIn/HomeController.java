5e7289451a0bcf5803f1fc141c08c4e5adf80486
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-27 17:55:43 +0100

    Refactor handle method

--- src/main/java/com/codecool_mjs/controller/webAccessController/logIn/HomeController.java
+++ src/main/java/com/codecool_mjs/controller/webAccessController/logIn/HomeController.java
@@ -22,34 +22,60 @@ public class HomeController implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        Boolean isNewSession = sessionController.isNewSession(httpExchange);
-        String method = httpExchange.getRequestMethod();
+        Boolean isSessionExist = false;
+        //check if cookie with session id exist in browser
+        try {
+            isSessionExist = sessionController.verifySession(httpExchange);
+        } catch (DaoException e) {
+            httpExchange.sendResponseHeaders(503, -1);
+        }
 
-        if (isNewSession && method.equals("GET")) {
-            String responseBody = templatesProcessor.ProcessTemplateToPage("index");
 
-            httpExchange.sendResponseHeaders(200, responseBody.getBytes().length);
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(responseBody.getBytes());
-            os.close();
-        } else if (isNewSession && method.equals("POST")) {
-            try {
-                User user = sessionController.logIn(httpExchange);
-            } catch (DaoException e) {
-                e.printStackTrace();
+        if (isSessionExist) {
+        //means session id is in db, and user session is established.
+        User user = sessionController.getLoggedUser();
+        sendUserController(httpExchange, user);
+
+        } else if (!isSessionExist) {
+            String method = httpExchange.getRequestMethod();
+
+            if (method.equals("GET")) {
+                sendLoginPage(httpExchange);
+
+            } else if (method.equals("POST")) {
+                try {
+                    //parse login form,  save session id to db, send cookie and redirect to proper user controller
+                    sessionController.logIn(httpExchange);
+                    User user = sessionController.getLoggedUser();
+                    sendUserController(httpExchange, user);
+                } catch (DaoException e) {
+                    httpExchange.sendResponseHeaders(503, -1);
+                }
             }
-        } else {
-            String userRole = sessionController.getLoggedUser().getProfession();
-            if (userRole.equals("Mentor")) {
-                httpExchange.getResponseHeaders().add("Location", "/mentor-home");
-                httpExchange.sendResponseHeaders(302, -1);
-            } else if (userRole.equals("Codecooler")) {
-                httpExchange.getResponseHeaders().add("Location", "/codecooler-home");
-                httpExchange.sendResponseHeaders(302, -1);
-            } else if (userRole.equals("Admin")) {
-                httpExchange.getResponseHeaders().add("Location", "/admin-home");
-                httpExchange.sendResponseHeaders(302, -1);
-            }
+        }
+    }
+
+    private void sendLoginPage(HttpExchange httpExchange) throws IOException {
+        String responseBody = templatesProcessor.ProcessTemplateToPage("index");
+
+        httpExchange.sendResponseHeaders(200, responseBody.getBytes().length);
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(responseBody.getBytes());
+        os.close();
+    }
+
+    private void sendUserController(HttpExchange httpExchange, User user) throws IOException {
+        String userRole = user.getProfession();
+
+        if (userRole.equals("Mentor")) {
+            httpExchange.getResponseHeaders().add("Location", "/mentor-home");
+            httpExchange.sendResponseHeaders(302, -1);
+        } else if (userRole.equals("Codecooler")) {
+            httpExchange.getResponseHeaders().add("Location", "/codecooler-home");
+            httpExchange.sendResponseHeaders(302, -1);
+        } else if (userRole.equals("Admin")) {
+            httpExchange.getResponseHeaders().add("Location", "/admin-home");
+            httpExchange.sendResponseHeaders(302, -1);
         }
     }
 }
