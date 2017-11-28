9f5265b0b2efc6e843115b109293a84bb357300e
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-28 10:59:59 +0100

    Make ArtifactConstroller singleton

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactController.java
@@ -1,4 +1,39 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+import com.codecool_mjs.dataaccess.dao.ArtifactDao;
+import com.codecool_mjs.dataaccess.dao.Dao;
+import com.codecool_mjs.dataaccess.dao.DaoException;
+import com.codecool_mjs.model.Artifact;
+
+import java.util.List;
+import java.util.Map;
+
 public class ArtifactController {
+
+    private Dao<Artifact> dao;
+    private static ArtifactController instance = null;
+
+    public ArtifactController() {
+        setDao();
+    }
+
+    private void setDao() {
+        try {
+            dao = new ArtifactDao();
+            ConnectionProvider.getInstance().connectionRequest(dao);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArtifactController getInstance() {
+        if (instance == null) {
+            instance = new ArtifactController();
+        }
+        return instance;
+    }
+
+
 }
+
