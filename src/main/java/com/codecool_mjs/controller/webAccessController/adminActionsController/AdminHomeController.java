aedab0d17e35580790d8605bea6b4e60e193ae69
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-28 19:13:33 +0100

    Implement Sessionable interface methods

--- src/main/java/com/codecool_mjs/controller/webAccessController/adminActionsController/AdminHomeController.java
+++ src/main/java/com/codecool_mjs/controller/webAccessController/adminActionsController/AdminHomeController.java
@@ -1,7 +1,10 @@
 package com.codecool_mjs.controller.webAccessController.adminActionsController;
 
+import com.codecool_mjs.controller.applicationActionsController.SessionController;
+import com.codecool_mjs.controller.webAccessController.Sessionable;
 import com.codecool_mjs.dataaccess.dao.DaoException;
 import com.codecool_mjs.model.Admin;
+import com.codecool_mjs.model.User;
 import com.codecool_mjs.view.webView.TemplatesProcessor;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -11,50 +14,54 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AdminHomeController implements HttpHandler{
+public class AdminHomeController implements HttpHandler, Sessionable{
 
     private TemplatesProcessor templatesProcessor;
-    private Admin loggedUser;
+    private SessionController sessionController;
 
     public AdminHomeController(){
         this.templatesProcessor = new TemplatesProcessor();
-    }
-
-    public void setLoggedUser(Admin loggedUser) {
-        this.loggedUser = loggedUser;
+        this.sessionController = new SessionController();
     }
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+
+        try {
+            boolean correctAccessRequest = sessionController.checkAccountAccess(httpExchange, getAccessType());
+            if (correctAccessRequest){
+                sendPageForPopperAccess(httpExchange);
+            }
+        } catch (DaoException e) {
+            httpExchange.sendResponseHeaders(500, -1);
+        }
+    }
+
+    @Override
+    public void sendPageForPopperAccess(HttpExchange httpExchange) throws IOException, DaoException{
         String responseBody;
         int responseCode;
 
-        try {
-            responseBody = showAdminHomePage();
-            responseCode = 200;
-        } catch (DaoException e) {
-            responseBody = "No such page";
-            responseCode = 404;
-        }
+        responseBody = showAdminHomePage();
+        responseCode = 200;
 
         httpExchange.sendResponseHeaders(responseCode, responseBody.getBytes().length);
         OutputStream os = httpExchange.getResponseBody();
         os.write(responseBody.getBytes());
         os.close();
-
     }
 
-    public String showAdminHomePage() throws DaoException {
-        //temporary example of logged user. To remove when sessions will be implemented
-        setLoggedUser(new Admin(15,"Janusz", "Kowal", "j.k@cc.pl", "typoweHasło"));
+    private String showAdminHomePage() throws DaoException {
 
         Map<String, Object> variables = new HashMap<>();
-
-        variables.put("user", loggedUser);
-
+        variables.put("user", sessionController.getLoggedUser());
         templatesProcessor.setVariables(variables);
-
         String page = templatesProcessor.ProcessTemplateToPage("/admin/home");
         return page;
+    }
+
+    @Override
+    public String getAccessType() {
+        return "Admin";
     }
 }
