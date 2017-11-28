9497a5e70b8a9f710018fc7c1df62a6cd864ca33
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-28 11:59:50 +0100

    Make TeamController singleton

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/TeamController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/TeamController.java
@@ -1,4 +1,32 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+import com.codecool_mjs.dataaccess.dao.Dao;
+import com.codecool_mjs.dataaccess.dao.DaoException;
+import com.codecool_mjs.dataaccess.dao.TeamDao;
+import com.codecool_mjs.model.Team;
+
 public class TeamController {
+    private Dao<Team> dao;
+    private static TeamController instance = null;
+
+    public TeamController() {
+        setDao();
+    }
+
+    private void setDao() {
+        try {
+            dao = new TeamDao();
+            ConnectionProvider.getInstance().connectionRequest(dao);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static TeamController getInstance() {
+        if (instance == null) {
+            instance = new TeamController();
+        }
+        return instance;
+    }
 }
