411a40494e185b3872854fca9503e4913ef70655
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-29 13:32:28 +0100

    Make GroupMembershipController singleton

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/GroupMembershipController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/GroupMembershipController.java
@@ -1,4 +1,33 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.ConnectionProvider;
+import com.codecool_mjs.dataaccess.dao.DaoException;
+import com.codecool_mjs.dataaccess.dao.GroupMembershipDao;
+import com.codecool_mjs.dataaccess.dao.MembershipDao;
+import com.codecool_mjs.model.GroupMembership;
+
 public class GroupMembershipController {
+
+    private MembershipDao<GroupMembership> dao;
+    private static GroupMembershipController instance = null;
+
+    private GroupMembershipController() {
+        setDao();
+    }
+
+    private void setDao() {
+        try {
+            dao = new GroupMembershipDao();
+            ConnectionProvider.getInstance().connectionRequest(dao);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static GroupMembershipController getInstance() {
+        if (instance == null) {
+            instance = new GroupMembershipController();
+        }
+        return instance;
+    }
 }
