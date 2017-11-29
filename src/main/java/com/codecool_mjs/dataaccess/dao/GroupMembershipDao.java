370ba09a9cc5d63c3350f773654120472a91b849
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-29 13:32:07 +0100

    Make GroupMembershipDao's controller public

--- src/main/java/com/codecool_mjs/dataaccess/dao/GroupMembershipDao.java
+++ src/main/java/com/codecool_mjs/dataaccess/dao/GroupMembershipDao.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class GroupMembershipDao extends MembershipDao<GroupMembership> {
 
-    GroupMembershipDao() {
+    public GroupMembershipDao() {
         super();
     }
 
