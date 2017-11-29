72e30b913cdf761186912a11096f0c291fea8c09
Mateusz Steliga <mateusz.steliga@codecool.com>
2017-11-29 13:30:42 +0100

    Refactor class to fulfill inherited WebActionControler class requirments

--- src/main/java/com/codecool_mjs/controller/webAccessController/mentorActionController/DeleteStudentActions.java
+++ src/main/java/com/codecool_mjs/controller/webAccessController/mentorActionController/DeleteStudentActions.java
@@ -1,102 +1,75 @@
 package com.codecool_mjs.controller.webAccessController.mentorActionController;
 
 import com.codecool_mjs.controller.applicationActionsController.CodecoolerController;
-import com.codecool_mjs.controller.applicationActionsController.MentorController;
+import com.codecool_mjs.controller.webAccessController.Sessionable;
+import com.codecool_mjs.controller.webAccessController.WebActionController;
 import com.codecool_mjs.dataaccess.dao.DaoException;
-import com.codecool_mjs.model.Admin;
 import com.codecool_mjs.model.Codecooler;
 import com.codecool_mjs.model.Mentor;
 import com.codecool_mjs.utilities.UriResolver;
-import com.codecool_mjs.view.webView.TemplatesProcessor;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
 
-public class DeleteStudentActions implements HttpHandler{
+public class DeleteStudentActions extends WebActionController implements Sessionable {
 
-    private TemplatesProcessor templateProcessor;
-    private Mentor loggedUser;
-    private CodecoolerController codecoolerController = CodecoolerController.getInstance();
-
+    private static String CONFIRMATION_TEMPLATE_URL = "mentor/delete-confirmation";
+    private static String DATA_TEMPLATE_URL = "mentor/delete-student";
+    private CodecoolerController codecoolerController;
 
     public DeleteStudentActions(){
-        this.templateProcessor = new TemplatesProcessor();
+        super();
+        codecoolerController = CodecoolerController.getInstance();
     }
 
-    public void setLoggedUser(Mentor loggedUser) {
-        this.loggedUser = loggedUser;
+
+    private String showStudentToDeleteAction(Integer id) throws DaoException {
+
+        Codecooler codecooler = codecoolerController.getCodecoolerById(id);
+        setVariable("student", codecooler);
+
+        return processTemplate(DATA_TEMPLATE_URL);
+    }
+
+    private String deleteMentorAction(Integer id) throws DaoException {
+
+        codecoolerController.deleteCodecooler(id);
+
+        String page = processTemplate(CONFIRMATION_TEMPLATE_URL);
+        return page;
     }
 
     @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
+    public String getAccessType() {
+        return "Mentor";
+    }
 
+    @Override
+    public void sendPageForProperAccess(HttpExchange httpExchange) throws IOException, DaoException {
         String responseBody = "";
         int responseCode = 200;
 
         String method = httpExchange.getRequestMethod();
         String idStr = UriResolver.getUserIdFromURI(httpExchange);
 
-        setLoggedUser(new Mentor(15,"Janusz", "Kowal", "j.k@cc.pl", "typoweHasło"));
-
         if(method.equals("GET")) {
 
             Integer id = Integer.parseInt(idStr);
 
-            responseBody = showStudentToDelete(id);
+            responseBody = showStudentToDeleteAction(id);
         }
 
         if(method.equals("POST")) {
 
             Integer id = Integer.parseInt(idStr);
 
-            responseBody = deleteMentor(id);
+            responseBody = deleteMentorAction(id);
         }
 
         httpExchange.sendResponseHeaders(responseCode, responseBody.getBytes().length);
         OutputStream os = httpExchange.getResponseBody();
         os.write(responseBody.getBytes());
         os.close();
-    }
-
-    private String showStudentToDelete(Integer id){
-
-        Codecooler codecooler = null;
-
-        Map<String, Object> variables = new HashMap<>();
-
-        try {
-            codecooler = codecoolerController.getCodecoolerById(id);
-        } catch (DaoException e) {
-            e.printStackTrace();
-        }
-
-        variables.put("user", loggedUser);
-        variables.put("student", codecooler);
-
-        templateProcessor.setVariables(variables);
-        String page = templateProcessor.ProcessTemplateToPage("mentor/delete-student");
-        return page;
-    }
-
-    private String deleteMentor(Integer id) {
-
-        try {
-            codecoolerController.deleteCodecooler(id);
-        } catch (DaoException e) {
-            e.printStackTrace();
-        }
-
-        Map<String, Object> variables = new HashMap<>();
-
-        variables.put("user", loggedUser);
-
-        templateProcessor.setVariables(variables);
-
-        String page = templateProcessor.ProcessTemplateToPage("mentor/delete-confirmation");
-        return page;
     }
 }
