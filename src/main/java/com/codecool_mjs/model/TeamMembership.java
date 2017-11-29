bddbf32752a25f25370594537c565c7183988607
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-29 11:31:29 +0100

    Implement TeamMembership methods

--- src/main/java/com/codecool_mjs/model/TeamMembership.java
+++ src/main/java/com/codecool_mjs/model/TeamMembership.java
@@ -1,4 +1,25 @@
 package com.codecool_mjs.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TeamMembership {
+
+    private List<Codecooler> codecoolers;
+    private Team team;
+
+    public TeamMembership(Team team) {
+        this.team = team;
+        this.codecoolers = new ArrayList<>();
+    }
+
+    public List<Codecooler> getCodecoolers() {
+        return codecoolers;
+    }
+
+    public void addCodecoolers(Codecooler codecooler) {
+        this.codecoolers.add(codecooler);
+    }
 }
+
+
