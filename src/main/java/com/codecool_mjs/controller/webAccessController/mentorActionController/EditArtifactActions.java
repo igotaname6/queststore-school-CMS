b3c0b4c8da46850714a4843788b1d6013ba02d03
Mateusz Steliga <mateusz.steliga@codecool.com>
2017-11-29 13:30:51 +0100

    Refactor class to fulfill inherited WebActionControler class requirments

--- src/main/java/com/codecool_mjs/controller/webAccessController/mentorActionController/EditArtifactActions.java
+++ src/main/java/com/codecool_mjs/controller/webAccessController/mentorActionController/EditArtifactActions.java
@@ -1,6 +1,8 @@
 package com.codecool_mjs.controller.webAccessController.mentorActionController;
 
 import com.codecool_mjs.controller.applicationActionsController.ArtifactController;
+import com.codecool_mjs.controller.webAccessController.Sessionable;
+import com.codecool_mjs.controller.webAccessController.WebActionController;
 import com.codecool_mjs.dataaccess.dao.DaoException;
 import com.codecool_mjs.model.Mentor;
 import com.codecool_mjs.model.Artifact;
@@ -17,23 +19,32 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EditArtifactActions implements HttpHandler{
+public class EditArtifactActions extends WebActionController implements Sessionable {
 
-    private TemplatesProcessor templateProcessor;
-    private Mentor loggedUser;
-    private ArtifactController artifactController = ArtifactController.getInstance();
+    private static String CONFIRMATION_TEMPLATE_URL = "mentor/edit-artifact";
+    private static String DATA_TEMPLATE_URL = "mentor/edit-artifact";
+    private ArtifactController artifactController;
 
     public EditArtifactActions(){
-        this.templateProcessor = new TemplatesProcessor();
+        super();
+        artifactController = ArtifactController.getInstance();
     }
 
-    public void setLoggedUser(Mentor loggedUser) {
-        this.loggedUser = loggedUser;
+    private String editArtifact(Integer id) throws DaoException {
+
+        Artifact artifact = artifactController.getArtifactById(id);
+        setVariable("artifact", artifact);
+
+        return processTemplate(DATA_TEMPLATE_URL);
     }
 
     @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
+    public String getAccessType() {
+        return "Mentor";
+    }
 
+    @Override
+    public void sendPageForProperAccess(HttpExchange httpExchange) throws IOException, DaoException {
         String responseBody = "";
         int responseCode = 200;
 
@@ -63,40 +74,13 @@ public class EditArtifactActions implements HttpHandler{
                 records.put("isGroup" , "false");
             }
 
-            try {
-                artifactController.editArtifact(records);
-            } catch (DaoException e) {
-                e.printStackTrace();
-            }
-
-            responseBody = templateProcessor.ProcessTemplateToPage("mentor/edit-confirmation");
+            artifactController.editArtifact(records);
+            responseBody = processTemplate(CONFIRMATION_TEMPLATE_URL);
         }
 
         httpExchange.sendResponseHeaders(responseCode, responseBody.getBytes().length);
         OutputStream os = httpExchange.getResponseBody();
         os.write(responseBody.getBytes());
         os.close();
-    }
-
-    private String editArtifact(Integer id){
-
-        Artifact artifact = null;
-
-        Mentor mentor = new Mentor(15,"Janusz", "Kowal", "j.k@cc.pl", "typoweHasło");
-        Map<String, Object> variables = new HashMap<>();
-
-        try {
-            artifact = artifactController.getArtifactById(id);
-        } catch (DaoException e) {
-            e.printStackTrace();
-        }
-
-        variables.put("user", mentor);
-        variables.put("artifact", artifact);
-
-        templateProcessor.setVariables(variables);
-        String page = templateProcessor.ProcessTemplateToPage("mentor/edit-artifact");
-
-        return page;
     }
 }
