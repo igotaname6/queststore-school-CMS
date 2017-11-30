7229a34a09a267866f2368e73b0dc1e735267ce0
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-30 13:00:58 +0100

    Implement ArtifactOwnerController methods

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactOwnerController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactOwnerController.java
@@ -1,4 +1,23 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
+import com.codecool_mjs.dataaccess.dao.ArtifactOwnerDao;
+import com.codecool_mjs.model.Artifact;
+import com.codecool_mjs.model.ArtifactOwner;
+import com.codecool_mjs.model.Codecooler;
+
 public class ArtifactOwnerController {
+
+    private ArtifactOwnerDao aod = new ArtifactOwnerDao();
+
+    public ArtifactOwnerDao getAod() {
+        return aod;
+    }
+
+    public void addArtifactOwner(Integer userId, Integer artifactId) {
+        Codecooler codecooler = new Codecooler(userId);
+        Artifact artifact = new Artifact(artifactId);
+        ArtifactOwner artifactOwner = new ArtifactOwner(codecooler, artifact);
+
+        aod.addArtifactOwner(artifactOwner);
+    }
 }
