5a5056748879f5745649f1eb905ef13118e92ff1
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-30 11:54:25 +0100

    Add addAchievers method in QuestAchieversDao

---
+++ src/main/java/com/codecool_mjs/dataaccess/dao/QuestAchieversDao.java
@@ -0,0 +1,47 @@
+package com.codecool_mjs.dataaccess.dao;
+
+import com.codecool_mjs.model.QuestAchievers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class QuestAchieversDao {
+
+    private Connection connection;
+
+    private static final String URL = "jdbc:sqlite:src/main/resources/queststore.db";
+    private static final String DRIVER_CLASS = "org.sqlite.JDBC";
+
+
+    public void setConnection() throws DaoException {
+        try {
+            Class.forName(DRIVER_CLASS);
+            this.connection = DriverManager.getConnection(URL);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new DaoException("Exception in setConnection in QuestAchieversDao");
+        }
+    }
+
+    public void addQuestAchiever(QuestAchievers questAchievers) {
+        PreparedStatement stmt;
+
+        String update =  "INSERT INTO quest_achievers (quest_id, achiever_id) VALUES (?, ?);";
+
+        try {
+            stmt = this.connection.prepareStatement(update);
+
+            stmt.setInt(1, questAchievers.getQuest().getId());
+            stmt.setInt(1, questAchievers.getCodecooler().getId());
+
+            stmt.executeUpdate();
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- src/main/java/com/codecool_mjs/dataaccess/dao/QuestOwnersDao.java
+++
@@ -1,4 +0,0 @@
-package com.codecool_mjs.dataaccess.dao;
-
-public class QuestOwnersDao {
-}
