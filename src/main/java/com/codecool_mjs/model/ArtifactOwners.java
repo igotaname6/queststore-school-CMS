28fbd50a6e8bf65994e0c5373e3e0d2bbc914c1e
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-30 12:51:25 +0100

    Implement ArtifactOwners class

--- src/main/java/com/codecool_mjs/model/ArtifactOwners.java
+++ src/main/java/com/codecool_mjs/model/ArtifactOwners.java
@@ -1,4 +1,20 @@
 package com.codecool_mjs.model;
 
 public class ArtifactOwners {
+
+    private Codecooler codecooler;
+    private Artifact artifact;
+
+    public ArtifactOwners(Codecooler codecooler, Artifact artifact) {
+        this.codecooler = codecooler;
+        this.artifact = artifact;
+    }
+
+    public Codecooler getCodecooler() {
+        return codecooler;
+    }
+
+    public Artifact getArtifact() {
+        return artifact;
+    }
 }
