7beb2872bc68590877f141a6a68657d6b1aed23b
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-11-30 11:51:12 +0100

    Implement QuestAchievers methods

--- src/main/java/com/codecool_mjs/model/QuestAchievers.java
+++ src/main/java/com/codecool_mjs/model/QuestAchievers.java
@@ -1,4 +1,20 @@
 package com.codecool_mjs.model;
 
 public class QuestAchievers {
+
+    private Codecooler codecooler;
+    private Quest quest;
+
+    public QuestAchievers(Codecooler codecooler, Quest quest) {
+        this.codecooler = codecooler;
+        this.quest = quest;
+    }
+
+    public Codecooler getCodecooler() {
+        return codecooler;
+    }
+
+    public Quest getQuest() {
+        return quest;
+    }
 }
