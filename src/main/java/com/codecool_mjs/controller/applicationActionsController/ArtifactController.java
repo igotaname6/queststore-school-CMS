8379487ebe52e9dbaa06968332a531fc35440a05
szymon.wojtas <szymon.wojtas@gmail.com>
2017-11-30 19:27:44 +0100

    Implement method getArtifactsOwnedByUser

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactController.java
@@ -2,17 +2,21 @@ package com.codecool_mjs.controller.applicationActionsController;
 
 import com.codecool_mjs.dataaccess.ConnectionProvider;
 import com.codecool_mjs.dataaccess.dao.ArtifactDao;
+import com.codecool_mjs.dataaccess.dao.ArtifactOwnerDao;
 import com.codecool_mjs.dataaccess.dao.Dao;
 import com.codecool_mjs.dataaccess.dao.DaoException;
 import com.codecool_mjs.model.Artifact;
+import com.codecool_mjs.model.ArtifactOwner;
 
 import java.util.List;
 import java.util.Map;
 
 public class ArtifactController {
 
-    private Dao<Artifact> dao;
+    private ArtifactDao dao;
+    private ArtifactOwnerDao artifactOwnerDao;
     private static ArtifactController instance = null;
+
 
     public ArtifactController() {
         setDao();
@@ -21,6 +25,8 @@ public class ArtifactController {
     private void setDao() {
         try {
             dao = new ArtifactDao();
+            artifactOwnerDao = new ArtifactOwnerDao();
+            artifactOwnerDao.setConnection();
             ConnectionProvider.getInstance().connectionRequest(dao);
         } catch (DaoException e) {
             e.printStackTrace();
@@ -84,6 +90,10 @@ public class ArtifactController {
         Artifact artifact = new Artifact(id, name, description, cost, isGroup, isUsed);
 
         this.dao.update(artifact);
+    }
+
+    public List<Artifact> getArtifactsOwnedByUser(int user_id) throws DaoException {
+        return dao.getArtifactsByUserId(user_id);
     }
 }
 
