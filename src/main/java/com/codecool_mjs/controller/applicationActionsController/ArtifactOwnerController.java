4f2e7885308c13ac0de1b81d61b56b245beab7ff
szymon.wojtas <szymon.wojtas@gmail.com>
2017-12-01 12:27:04 +0100

    Change signature of addArtifactOwner method.

--- src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactOwnerController.java
+++ src/main/java/com/codecool_mjs/controller/applicationActionsController/ArtifactOwnerController.java
@@ -1,6 +1,7 @@
 package com.codecool_mjs.controller.applicationActionsController;
 
 import com.codecool_mjs.dataaccess.dao.ArtifactOwnerDao;
+import com.codecool_mjs.dataaccess.dao.DaoException;
 import com.codecool_mjs.model.Artifact;
 import com.codecool_mjs.model.ArtifactOwner;
 import com.codecool_mjs.model.Codecooler;
@@ -13,7 +14,7 @@ public class ArtifactOwnerController {
         return dao;
     }
 
-    public void addArtifactOwner(Integer userId, Integer artifactId) {
+    public void addArtifactOwner(Integer userId, Integer artifactId) throws DaoException {
         Codecooler codecooler = new Codecooler(userId);
         Artifact artifact = new Artifact(artifactId);
         ArtifactOwner artifactOwner = new ArtifactOwner(codecooler, artifact);
